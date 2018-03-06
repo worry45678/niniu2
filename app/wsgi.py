849e3c4dc4af4a91668f6c9db7eba1f67168e321
worry45678 <worry45678@126.com>
2018-03-06 08:09:32 +0800

    added: uwsgi

---
+++ app/wsgi.py
@@ -0,0 +1,8 @@
+import os
+
+from app import create_app
+
+# Create an application instance that web servers can use. We store it as
+# "application" (the wsgi default) and also the much shorter and convenient
+# "app".
+application = app = create_app(os.getenv('PYTHON_CONFIG') or 'default')
